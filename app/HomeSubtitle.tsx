3bb5fbca71507b8a22c999b157c4998d63ec40e3
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2026-07-13 22:35:18 +0200

    Manually shuffle home array to ensure all lines show

--- app/HomeSubtitle.tsx
+++ app/HomeSubtitle.tsx
@@ -8,19 +8,18 @@ export function HomeSubtitle() {
 
     useEffect(() => {
         const typed = new Typed(el.current, {
-            strings: [
+            strings: shuffleArray([
                 "Junior Software Developer",
                 "Junior Front-end Developer",
                 "Junior Back-end Developer",
                 "Cat lover",
                 "Tech enthusiast",
-            ],
+            ]),
             typeSpeed: 50,
             backSpeed: 50,
             backDelay: 3000,
             loop: true,
-            smartBackspace: true,
-            shuffle: true
+            smartBackspace: true
         });
         return () => {
             typed.destroy();
@@ -32,4 +31,14 @@ export function HomeSubtitle() {
             <span ref={el}></span>
         </h2>
     );
+}
+
+function shuffleArray<T>(array: Array<T>): Array<T> {
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
 }
