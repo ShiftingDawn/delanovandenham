9826e47e1b21c281117ca43ec6e76d4057f04b57
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2026-07-13 21:54:01 +0200

    Add jobs to resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -6,6 +6,38 @@ export default function PageResume() {
     return (
         <Page title={"Resume"}>
             <h2>Work history</h2>
+            <div className={"flex flex-col gap-8"}>
+                <JobCard
+                    company={"MediaMarkt"}
+                    jobTitle={"Fulfillment employee"}
+                    when={[["January", 2026], ["", -1]]}
+                    image={
+                        <Image src={"/resume/mediamarkt.svg"} alt={"Media Markt logo"} width={180} height={60}/>
+                    }/>
+                <JobCard
+                    company={"Aldi"}
+                    jobTitle={"First deputy branch manager"}
+                    when={[["November", 2022], ["May", 2025]]}
+                    image={
+                        <Image src={"/resume/aldi.svg"} alt={"Aldi logo"} width={60} height={64}/>
+                    }/>
+                <JobCard
+                    company={"Aldi"}
+                    jobTitle={"Second deputy branch manager"}
+                    description={"Promoted to first degree branch manager"}
+                    when={[["Januari", 2019], ["October", 2022]]}
+                    image={
+                        <Image src={"/resume/aldi.svg"} alt={"Aldi logo"} width={60} height={64}/>
+                    }/>
+                <JobCard
+                    company={"Aldi"}
+                    jobTitle={"Sales employee"}
+                    description={"Promoted to second degree branch manager"}
+                    when={[["December", 2017], ["December", 2018]]}
+                    image={
+                        <Image src={"/resume/aldi.svg"} alt={"Aldi logo"} width={60} height={64}/>
+                    }/>
+            </div>
             <h2>Internships</h2>
             <h2>Education</h2>
             <div className={"flex flex-col gap-8"}>
@@ -41,6 +73,32 @@ export default function PageResume() {
     );
 }
 
+interface JobCardProps {
+    company: string;
+    jobTitle: string;
+    when: [[string, number], [string, number]];
+    description?: string;
+    image: ReactNode
+}
+
+function JobCard({company, jobTitle, when, description, image}: JobCardProps) {
+    return (
+        <div className={"w-full max-w-lg mx-auto bg-ctp-surface0 p-4 rounded-lg"}>
+            <div className={"flex flex-col md:flex-row md:justify-between"}>
+                <p className={"text-xl font-bold text-accent"}>
+                    {jobTitle}
+                </p>
+                <div className={"grow-0 shrink-0"}>
+                    {image}
+                </div>
+            </div>
+            <p>{company}</p>
+            <p>{when[0][0]} {when[0][1]} - {when[1][1] === -1 ? "current" : `${when[1][0]} ${when[1][1]}`}</p>
+            {description && <p><i>{description}</i></p>}
+        </div>
+    );
+}
+
 interface SchoolCardProps {
     name: string;
     subname: string;
@@ -52,7 +110,7 @@ interface SchoolCardProps {
 
 function SchoolCard({name, subname, school, schoolLocation, description, image}: Readonly<SchoolCardProps>) {
     return (
-        <div className={"max-w-lg mx-auto bg-ctp-surface0 p-4 rounded-lg"}>
+        <div className={"w-full max-w-lg mx-auto bg-ctp-surface0 p-4 rounded-lg"}>
             <div className={"flex flex-col md:flex-row md:justify-between"}>
                 <p className={"text-xl font-bold text-accent"}>
                     {name}
