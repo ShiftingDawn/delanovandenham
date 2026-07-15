c1bb0643a294667a1c59f053f0be3d9b80546f66
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2026-07-15 07:56:35 +0200

    Move everything to src folder

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
         <footer className={"flex items-center justify-between px-4 bg-ctp-crust h-12"}>
             <p>
-                &copy; {new Date().getFullYear()} Delano van den Ham
+                &copy; {new Date().getFullYear()} Delano van den Ham. All rights reserved.
             </p>
         </footer>
     )
