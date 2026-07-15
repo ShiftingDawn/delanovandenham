cf5c3f4d639039d316255d689c06d9bf8a264d67
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2026-07-15 07:57:34 +0200

    Removed unused dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,6 @@
 import type {NextConfig} from "next";
-import createMDX from "@next/mdx";
 
-const nextConfig: NextConfig = {
-    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-};
-
-const withMDX = createMDX({
-    extension: /\.(md|mdx)$/
-})
-
-export default withMDX({
-    ...nextConfig,
-    experimental: {
-        mdxRs: true,
-    }
-});
+const nextConfig: NextConfig = {};
+export default nextConfig;
 
 import("@opennextjs/cloudflare").then(m => m.initOpenNextCloudflareForDev());
